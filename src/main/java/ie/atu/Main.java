3d4f2538c17260ef641852f492121df2ccff0660
NathanMcCormack <G00414780@atu.ie>
2023-11-20 10:46:48 +0000

    user prompt to enter a file name. File is created and text is added.

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,8 +14,23 @@ public class Main {
 
         //Create a File object
         File myFile = new File(fileName);
+
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Enter file name: ");
+        String fileInput = myScan.nextLine();
+
         String fileName2 = "File11.txt";
 
+        try(PrintWriter writer = new PrintWriter(new FileWriter(fileInput, true))){ //setting this to true keeps the old text printed to the file
+            //Notice there is no close method called. This is a try with resources
+
+            writer.println("Text for file. ");
+            System.out.println("Successfully printed to new file");
+        }
+        catch(IOException e){
+            System.out.println("Error occured");
+            e.printStackTrace();
+        }
 
         try(PrintWriter writer = new PrintWriter(new FileWriter(fileName2, true))){ //setting this to true keeps the old text printed to the file
             //Notice there is no close method called. This is a try with resources
@@ -23,7 +39,7 @@ public class Main {
             System.out.println("Successfully printed to file");
         }
         catch(IOException e){
-            System.out.println("Error occured");
+            System.out.println("Error occurred");
             e.printStackTrace();
         }
 
